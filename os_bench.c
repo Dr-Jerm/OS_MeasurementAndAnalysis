8080ff74467a063ce5848e0c7e55c548c29277a8
Jeremy R. Bernstein <utilityjerm@gmail.com>
2013-05-01 18:02:45 -0600

    Made good progress with the process context switching. Just need to figure out how to time it correctly. Shouldn't be too hard now that the pipes are all wired up. Also need to configure to run on one processor per the hint.~

--- os_bench.c
+++ os_bench.c
@@ -64,7 +64,7 @@ typedef struct{
 
 // Tiny function to time thread creation
 void *threadBench(Args* args){
-  args->stat->totalDelta+= timeNanoSec(&timer) - *(args->startF);
+//  args->stat->totalDelta+= timeNanoSec(&timer) - *(args->startF);
   pthread_exit(NULL);
 }
 
@@ -83,8 +83,8 @@ void createThreadBench(Stat* stat){
     args.startF = &startF;
 
     pthread_create(&thread, NULL, threadBench, &args);
+    stat->totalDelta += timeNanoSec(&timer) - startF;
     pthread_join(thread, NULL);
-//    stat->totalDelta += timeNanoSec(&timer) - startF;
   }
 
   stat->endTime = timeNanoSec(&timer);
@@ -105,53 +105,135 @@ void createProcessBench(Stat* stat){
       exit(1);
     } else if (rc == 0) {
       // child (new process)
-      stat->totalDelta += timeNanoSec(&timer) - startF;
+//      stat->totalDelta += timeNanoSec(&timer) - startF;
+        exit(0);
     } else {
       // parent goes down this path (original process)
-      exit(0);
+      stat->totalDelta += timeNanoSec(&timer) - startF;
+//      exit(0);
+      int wc = wait(NULL);
     }
-//    stat->totalDelta += timeNanoSec(&timer) - startF;
   }
 
   stat->endTime = timeNanoSec(&timer);
 }
 
+// Benchmark for Context Switching Processes
+void switchProcessBench(Stat* stat){
+  stat->startTime = timeNanoSec(&timer);
+
+  int pipeA[2];
+  int pipeB[2];
+  int ping = 1;
+
+  if (pipe(pipeA)==-1) {
+    perror("Pipe pipeA failed.\n");
+    return;
+  }
+  if (pipe(pipeB)==-1) {
+    perror("Pipe pipeB failed.\n");
+    return;
+  }
+  
+  int rc = fork();
+  if (rc < 0) {
+    // fork failed; exit
+    fprintf(stderr, "fork failed\n");
+    exit(1);
+  } else if (rc == 0) {
+    // child (new process)
+    while( ping == 1){
+      read(pipeA[0], &ping, sizeof(ping));
+      write(pipeB[1], &ping, sizeof(ping));
+    }
+    exit(0);
+    
+  } else {
+    // parent goes down this path (original process)
+    int count = 0;
+    while (count < conf.iterations){
+      write(pipeA[1], &ping, sizeof(ping));
+      read(pipeB[0], &ping, sizeof(ping));
+      count++;
+    }
+    ping = -1;
+    write(pipeA[1], &ping, sizeof(ping));
+    wait(NULL);
+  }
+ 
+  stat->endTime = timeNanoSec(&timer);
+}
+
+
 
 void main( int argc, char **argv) {
   conf.iterations = 10000000;
-  
-  // Testing non-System function calls
-  Stat* nonSysFuncStat = malloc(sizeof(Stat));;
-  char* nonSysFuncName = "Non-System Function Benchmark";
-  nonSysFuncStat->testName = nonSysFuncName;
-  nonSysFuncBench(nonSysFuncStat);
-  printStats(nonSysFuncStat, &conf);
-  free(nonSysFuncStat);
 
-  // Testing System function calls
-  Stat* sysFuncStat = malloc(sizeof(Stat));
-  char* sysFuncName = "System Function Benchmark";
-  sysFuncStat->testName = sysFuncName;
-  sysFuncBench(sysFuncStat);
-  printStats(sysFuncStat, &conf);
-  free(sysFuncStat);
+  int test = 0;
+  if (argc == 2){
+    test = atoi(argv[1]);
+  }
+ 
+  switch(test) {
+    case 0: // RUN ALL
+     
+    case 1:
+      ;// Testing non-System function calls
+      Stat* nonSysFuncStat = malloc(sizeof(Stat));
+      nonSysFuncStat-> totalDelta = 0;
+      char* nonSysFuncName = "Non-System Function Benchmark";
+      nonSysFuncStat->testName = nonSysFuncName;
+      nonSysFuncBench(nonSysFuncStat);
+      printStats(nonSysFuncStat, &conf);
+      free(nonSysFuncStat);
+      if ( test > 0 ){ break; }
 
-   conf.iterations = 5000;
-  // Testing process creation
-  Stat* createThreadStat = malloc(sizeof(Stat));
-  char* createThreadName = "Thread Creation Benchmark";
-  createThreadStat->testName = createThreadName;
-  createThreadBench(createThreadStat);
-  printStats(createThreadStat, &conf);
-  free(createThreadStat);
+    case 2:
+      ;// Testing System function calls
+      Stat* sysFuncStat = malloc(sizeof(Stat));
+      sysFuncStat->totalDelta = 0;
+      char* sysFuncName = "System Function Benchmark";
+      sysFuncStat->testName = sysFuncName;
+      sysFuncBench(sysFuncStat);
+      printStats(sysFuncStat, &conf);
+      free(sysFuncStat);
+      if ( test > 0 ){ break; }
 
+    case 3:
+      conf.iterations = 10000;
+      ;// Testing process creation
+      Stat* createThreadStat = malloc(sizeof(Stat));
+      createThreadStat-> totalDelta = 0;
+      char* createThreadName = "Thread Creation Benchmark";
+      createThreadStat->testName = createThreadName;
+      createThreadBench(createThreadStat);
+      printStats(createThreadStat, &conf);
+      free(createThreadStat);
+      if ( test > 0 ){ break; }
 
-  conf.iterations = 5000;
-  // Testing process creation
-  Stat* createProcessStat = malloc(sizeof(Stat));
-  char* createProcessName = "Process Creation Benchmark";
-  createProcessStat->testName = createProcessName;
-  createProcessBench(createProcessStat);
-  printStats(createProcessStat, &conf);
-  free(createProcessStat);
+    case 4:
+      conf.iterations = 10000;
+      ;// Testing process creation
+      Stat* createProcessStat = malloc(sizeof(Stat));
+      createProcessStat->totalDelta = 0;
+      char* createProcessName = "Process Creation Benchmark";
+      createProcessStat->testName = createProcessName;
+      createProcessBench(createProcessStat);
+      printStats(createProcessStat, &conf);
+      free(createProcessStat);
+      if ( test > 0 ){ break; }
+
+    case 5:
+      conf.iterations = 100000;
+      ;// Testing process context switching
+      Stat* switchProcessStat = malloc(sizeof(Stat));
+      switchProcessStat->totalDelta = 0;
+      char* switchProcessName = "Process Context Switch Benchmark";
+      switchProcessStat->testName = switchProcessName;
+      switchProcessBench(switchProcessStat);
+      printStats(switchProcessStat, &conf);
+      free(switchProcessStat);
+      if ( test > 0 ){ break; }
+  }
+
 }
